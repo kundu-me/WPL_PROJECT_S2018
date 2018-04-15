fe84cadeb65dbec117f985fee6b95b8d3f26fc90
nxkundu <Pass@1234>
2018-04-15 02:47:44 -0500

    Password reset message wrong

--- sconnect/profile/course_box.php
+++ sconnect/profile/course_box.php
@@ -20,15 +20,6 @@ if ($db->connect_error) {
 	<meta charset="UTF-8">
 	<title>Add New Course Interface</title>
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="course_box.css">
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="course_box.js"></script> 
-
 	
 </head>
 
