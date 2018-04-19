fbeb9215b298e5932bc0d054988a3d2440f17629
nxkundu <Pass@1234>
2018-04-19 11:09:00 -0500

    Added Left Card and Right Card

---
+++ sconnect/card_left/index.php
@@ -0,0 +1,13 @@
+
+<!--
+    @author: Nirmallya Kundu <nxkundu@gmail.com>
+    @page: Account Verification UI
+    @description: This page verifies the user with the email and OTP
+-->
+
+<div class="row" style="text-align: center;">
+	<img src="../user_data/profile_image/sample.jpg" style="width:100; height: 300px;">
+	<br>
+	<br>
+	<span class="sconnect-profile-header"><h3><?php echo " " . $_SESSION['fname'] . " " . $_SESSION['lname'] ?> </h3></span>
+</div>
