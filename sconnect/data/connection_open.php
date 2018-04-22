bf01e6033bde61967786b65a607e1acd5da66eab
abhi06548 <abhi06548@yahoo.com>
2018-04-22 04:36:14 -0500

    Message dynamic loading fucntionality

--- sconnect/data/connection_open.php
+++ sconnect/data/connection_open.php
@@ -8,16 +8,16 @@
   *
   */
 
-// $sql_servername = "kundu.me";
-// $sql_username = "kundujwg_sc";
-// $sql_password = "Pass@1234";
-// $sql_database = "kundujwg_sconnect_db1";
+ $sql_servername = "kundu.me";
+ $sql_username = "kundujwg_sc";
+ $sql_password = "Pass@1234";
+ $sql_database = "kundujwg_sconnect_db1";
 
 
-$sql_servername = "localhost";
+/*$sql_servername = "localhost";
 $sql_username = "root";
 $sql_password = "";
-$sql_database = "sconnect";
+$sql_database = "sconnect";*/
 
 // Create connection
 $sql_connection = mysqli_connect($sql_servername, $sql_username, $sql_password, $sql_database);
