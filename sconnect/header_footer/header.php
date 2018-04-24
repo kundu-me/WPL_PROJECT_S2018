ab444f7d240c4311379d1805c7c7238411f43a6a
nxkundu <Pass@1234>
2018-04-24 00:16:11 -0500

    Updated css

--- sconnect/header_footer/header.php
+++ sconnect/header_footer/header.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="utf-8">
     <title>SConnect</title>
-    <link rel="shortcut icon" href="static/img/sconnect-logo-3.jpg" type="image/x-icon">
+    <link rel="shortcut icon" href="../static/img/sconnect-logo-3.jpg" type="image/x-icon">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
